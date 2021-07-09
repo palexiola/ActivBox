2bc88f7fbdbaba343796a2615b894f4426580ae3
palexiola <86502931+palexiola@users.noreply.github.com>
2021-07-09 23:43:04 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,63 @@
+$(function() {
+     
+        /*Fixed Header */
+        let header = $("#header");
+        let intro = $("#intro");
+        let introH = intro.innerHeight(); 
+        let scrollPos = $(window).scrollTop();
+        let nav = $("#nav");
+        let navToggle = $("#navToggle");
+
+        $(window).on("scroll load resize", function() {
+            introH = intro.innerHeight (); 
+            scrollPos =$(this).scrollTop();
+
+            if( scrollPos > introH ) {
+                header.addClass("fixed");
+            } else {
+                header.removeClass("fixed");
+            }
+
+        });
+
+
+        /*Smoth scroll */
+        $("[data-scroll]").on ("click", function(event) {
+            event.preventDefault();
+
+            let elementID = $(this).data('scroll');
+            let elementOffset = $(elementID).offset().top;
+
+            nav.removeClass("show");
+
+            $("html, body").animate({
+                scrollTop: elementOffset - 55
+            }, 700);
+
+
+        });
+
+
+        /* Nav Toggle */
+
+        navToggle.on("click", function(event) {
+            event.preventDefault();
+
+            nav.toggleClass("show");
+
+        });
+
+        /* Reviews: http://kenwheeler.github.io/slick/ */
+        let slider = $("#reviewsSlider");
+
+
+        slider.slick({
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            fade: true,
+            arrows: true,
+            dots: true
+        }); 
+
+});
